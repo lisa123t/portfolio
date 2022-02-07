c5bff5d4a9a3164f1e9c4ac2c67fe9481bcb11c5
lisa123t <98433656+lisa123t@users.noreply.github.com>
2022-02-06 20:56:21 -0500

    Add files via upload

---
+++ number-tricks.js
@@ -0,0 +1,27 @@
+/*  Lisa Tomasetti  */
+/*  02-06-2022  */
+
+/* Get input */
+let someNumber = prompt("Pick a number, any number: "); 
+/* save the number for later */ 
+someNumber = startNumber;  
+
+/* add 9 to your number */
+someNumber += 9; 
+document.write("If you add 9 to your number you get: ", someNumber); 
+
+/* mulitply the number by 2 */
+someNumber *= 2; 
+document.write("Multiply by 2 and you have: ", someNumber); 
+
+/* subtract by 4 */
+someNumber -= 4; 
+document.write("Subtract 4 and get: ", someNumber); 
+
+/* divide by 2 */ 
+someNumber // 2; 
+document.write("Divide by 2 and you have: ", someNumber); 
+
+/* subtract the inital number */ 
+someNumber -= startNumber; 
+document.write("Subract the number you first chose and you get: ", someNumber); 
